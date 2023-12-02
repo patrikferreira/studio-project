9a816159874b460ceadfef721b844b1d2c3a7bc9
Patrik Ferreira <patrik.andradef@gmail.com>
2023-12-02 15:03:51 -0300

    [FEATURE]: Adicionar botão de voltar

--- src/app/components/StudioHeader.tsx
+++ src/app/components/StudioHeader.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import style from '../(aplication)/page.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function StudioHeader() {
+  const router = useRouter();
+
+  function goBack() {
+    router.back();
+  }
+  
   return (
     <div className={style.studioHeader}>
-        <div className={style.imgStudioHeader}>
+        <div className={style.divReturn} onClick={goBack}>
+          <div className={style.imgStudioHeader}>
             <img src="assets/ficticia-perfil.png" alt="" />
+          </div>
+          <p>Malima Stúdio</p>
         </div>
-        <p>Malima Stúdio</p>
     </div>
   )
 }
