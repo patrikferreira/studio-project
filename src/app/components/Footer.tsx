c144534d555b225a0aefc56e3345a95893e52ade
Patrik Ferreira <patrik.andradef@gmail.com>
2023-12-07 21:03:11 -0300

    [CHORE]: ajustes na homepage

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
                 redirectRoute(item.routePath)
               }}>
                 <i className={item.classNameIcon}></i>
-                <p>{item.titleItem}</p>
+                {/* <p>{item.titleItem}</p> */}
               </div>
             )
           })
