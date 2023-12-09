f38e4e789067b7b1c639b338ab41df457a541217
Patrik Ferreira <patrik.andradef@gmail.com>
2023-12-09 18:09:01 -0300

    [FEATURE]: adicionar botao voltar

--- src/app/components/StudioHeader.tsx
+++ src/app/components/StudioHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '../(aplication)/page.module.css'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
@@ -6,19 +6,32 @@ import NameStudio from './NameStudio';
 
 export default function StudioHeader() {
   const router = useRouter();
+  const [changeStyles, setChangeStyles] = useState<boolean>(false);
 
   function goBack() {
-    router.back();
+    router.back()
   }
-  
+
+  useEffect(() => {
+    setTimeout(() => {
+      setChangeStyles(true)
+    }, 5000);
+    
+  }, [])
+
   return (
     <div className={style.studioHeader}>
-        <div className={style.divReturn} onClick={goBack}>
-          <div className={style.imgStudioHeader}>
-            <img src="assets/ficticia-perfil.png" alt="" />
-          </div>
-          <NameStudio />
+      <div className={`${style.divReturn} ${changeStyles && style.changeStyle}`} onClick={goBack}>
+        <i className="fa-solid fa-chevron-left"></i>
+        <span className={`${changeStyles && style.changeStyleSpan}`}>Voltar</span>
+      </div>
+      
+      <div className={style.divStudio}>
+        <div className={style.imgStudioHeader}>
+          <img src="assets/ficticia-perfil.png" alt="" />
         </div>
+        <NameStudio />
+      </div>
     </div>
   )
 }
